2eb8aa8efef48d6defe402826d3d96fabcd259c8
Tailung1 <ramazsamitashvili11@gmail.com>
2025-05-27 17:30:59 +0400

    feat:serve home route and return JSON data on /posts

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+import http from "http";
+import fs from "fs"
 
+const server = http.createServer((req, res) => {
+  if(req.url==="/") {
+    res.writeHead(200,{"Content-Type":"text/plain"})
+    res.end("home")
+  } else if (req.url==="/posts") {
+    const posts=fs.readFileSync("posts.json","utf8")
+    res.writeHead(200,{"Content-Type":"application/json"})
+    res.end(posts)
+  }
+
+});
+
+server.listen(4040, "localhost", () => {
+  console.log("Server is running on http://localhost:4040");
+});
